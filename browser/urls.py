a30324166740bdffb1af2b1d77f9a7d497f1c9e5
abakarovaMarina <abakamarina@gmail.com>
2024-11-06 15:58:41 +0100

    update

--- browser/urls.py
+++ browser/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import search_view, results
+from .views import search_view, results, download_file
 
 urlpatterns = [
     path('', search_view, name='search'),
     path('search/', search_view, name='search'),
     path('results/', results, name='results'),
+    path('download/<str:fbpp_id>/', download_file, name='download_file'),
 ]
--- browser/views.py
+++ browser/views.py
@@ -2,8 +2,10 @@ import os
 import pandas as pd
 import plotly.express as px
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, FileResponse
 
+
+dir ="FBpp_example/"  
 
 def search_view(request):
     return render(request, 'browser/search.html')
@@ -14,22 +16,32 @@ def results(request):
     if not fbpp_id:
         return HttpResponse("Please provide a FBpp ID.")
 
-    file_path = f"/Users/manchuta/Documents/GitHub/Droso_GEMMEwebsite/FBpp_example/{fbpp_id}/{fbpp_id}_normPred_evolCombi.txt"
+    file_path = f"{dir}{fbpp_id}/{fbpp_id}_normPred_evolCombi.txt"
     if not os.path.exists(file_path):
         return HttpResponse("File not found.")
 
     # Read the file into a DataFrame
     df = pd.read_csv(file_path, skiprows=1, sep=' ', header=None, index_col=0)
-
+    reversed_oranges = px.colors.sequential.Oranges[::-1]
     # Generate the heatmap using Plotly
-    fig = px.imshow(df, color_continuous_scale='Oranges', title=f"Heatmap for {fbpp_id}")
+    fig = px.imshow(df, color_continuous_scale=reversed_oranges, title=f"GEMME mutational landscape for {fbpp_id}")
+    fig.update_layout(title_x=0.5)
     fig.update_layout(
         autosize=False,
-        width=800,  # Adjust the width as needed
-        height=400,  # Adjust the height as needed
-        margin=dict(l=50, r=50, b=100, t=100, pad=4)
+        width=1500,  # Adjust the width as needed
+        height=500,  # Adjust the height as needed
     )
     # Convert the plotly figure to HTML
+    fig.update_yaxes(tickfont=dict(size=14), dtick=1)  # Increase the yticks size and ensure all ticks are visible
     heatmap_html = fig.to_html(full_html=False)
 
-    return render(request, 'browser/results.html', {'heatmap_html': heatmap_html, 'query': fbpp_id})
+    return render(request, 'browser/results.html', {'heatmap_html': heatmap_html, 'query': fbpp_id, 'file_path': file_path})
+
+def download_file(request, fbpp_id):
+    file_path = f"{dir}{fbpp_id}/{fbpp_id}_normPred_evolCombi.txt"
+    if not os.path.exists(file_path):
+        return HttpResponse("File not found.")
+    
+    response = FileResponse(open(file_path, 'rb'))
+    response['Content-Disposition'] = f'attachment; filename="{fbpp_id}_normPred_evolCombi.txt"'
+    return response
