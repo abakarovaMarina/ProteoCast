fe714589f099ecc3d67aab7b54aef621f53b4b8b
Maria Ines Freiberger <37904042+mariafreiberger@users.noreply.github.com>
2024-11-13 09:40:27 -0300

    Add files via upload

--- browser/urls.py
+++ browser/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import search_view, results, download_file
+from .views import search_view, results_view, download_file
 
 urlpatterns = [
-    path('', search_view, name='search'),
+    path('results/download/<str:fbpp_id>/', download_file, name='download_file'),
     path('search/', search_view, name='search'),
-    path('results/', results, name='results'),
-    path('download/<str:fbpp_id>/', download_file, name='download_file'),
+    path('results/', results_view, name='results'),
 ]
--- browser/views.py
+++ browser/views.py
@@ -2,43 +2,62 @@ import os
 import pandas as pd
 import plotly.express as px
 from django.shortcuts import render
+from django.conf import settings
 from django.http import HttpResponse, FileResponse
 
-
-dir ="FBpp_example/"  
 
 def search_view(request):
     return render(request, 'browser/search.html')
 
 
-def results(request):
+def results_view(request):
     fbpp_id = request.GET.get('q')
     if not fbpp_id:
         return HttpResponse("Please provide a FBpp ID.")
 
-    file_path = f"{dir}{fbpp_id}/{fbpp_id}_normPred_evolCombi.txt"
-    if not os.path.exists(file_path):
+    path = f'jobs/{fbpp_id}/{fbpp_id}_normPred_evolCombi.txt'
+    full_path = os.path.join(settings.BASE_DIR, 'browser', 'static', path)
+    if not os.path.exists(full_path):
         return HttpResponse("File not found.")
 
-    # Read the file into a DataFrame
-    df = pd.read_csv(file_path, skiprows=1, sep=' ', header=None, index_col=0)
+    # Leer el archivo en un DataFrame
+    df = pd.read_csv(full_path, skiprows=1, sep=' ', header=None, index_col=0)
     reversed_oranges = px.colors.sequential.Oranges[::-1]
-    # Generate the heatmap using Plotly
+    
+    # Generar el heatmap usando Plotly
     fig = px.imshow(df, color_continuous_scale=reversed_oranges, title=f"GEMME mutational landscape for {fbpp_id}")
     fig.update_layout(title_x=0.5)
     fig.update_layout(
         autosize=False,
-        width=1500,  # Adjust the width as needed
-        height=500,  # Adjust the height as needed
+        width=1500,
+        height=500,
     )
-    # Convert the plotly figure to HTML
-    fig.update_yaxes(tickfont=dict(size=14), dtick=1)  # Increase the yticks size and ensure all ticks are visible
+    fig.update_yaxes(tickfont=dict(size=14), dtick=1)  # Aumentar tamaño de los ticks en el eje Y
     heatmap_html = fig.to_html(full_html=False)
 
-    return render(request, 'browser/results.html', {'heatmap_html': heatmap_html, 'query': fbpp_id, 'file_path': file_path})
+    # Añadir las URLs de las imágenes al contexto
+    image_path_1 = os.path.join(settings.BASE_DIR, 'browser', 'static', f'jobs/{fbpp_id}/{fbpp_id}_GMM.jpg')
+    image_path_2 = os.path.join(settings.BASE_DIR, 'browser', 'static', f'jobs/{fbpp_id}/GMM.png')
+
+    image_url_1 = None
+    image_url_2 = None
+    if os.path.exists(image_path_1):
+        image_url_1 = f'/static/jobs/{fbpp_id}/{fbpp_id}_GMM.jpg'
+    if os.path.exists(image_path_2):
+        image_url_2 = f'/static/jobs/{fbpp_id}/GMM.png'
+
+    return render(request, 'browser/results.html', {
+        'heatmap_html': heatmap_html,
+        'query': fbpp_id,
+        'file_path': full_path,
+        'image_url_1': image_url_1,
+        'image_url_2': image_url_2
+    })
+
+
 
 def download_file(request, fbpp_id):
-    file_path = f"{dir}{fbpp_id}/{fbpp_id}_normPred_evolCombi.txt"
+    file_path = f"jobs/{fbpp_id}/{fbpp_id}_normPred_evolCombi.txt"
     if not os.path.exists(file_path):
         return HttpResponse("File not found.")
     
