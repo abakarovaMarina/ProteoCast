efe81bfcc270faeff6c14df6d2471111cfcf2c1c
Maria Ines Freiberger <37904042+mariafreiberger@users.noreply.github.com>
2024-11-13 09:38:04 -0300

    Add files via upload

--- MarinaWeb/views.py
+++ MarinaWeb/views.py
@@ -1,26 +1,5 @@
+# browser/views.py
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def search_view(request):
     return render(request, 'browser/search.html')
-
-def results_view(request):
-    query = request.GET.get('q') 
-    
-    results = [
-        {"title": "Resultado 1", "description": "Description 1."},
-        {"title": "Resultado 2", "description": "Description 2."},
-        {"title": "Resultado 3", "description": "Description 3."},
-    ]
-
-    if query:
-        filtered_results = [result for result in results if query.lower() in result["title"].lower()]
-    else:
-        filtered_results = results 
-
-    context = {
-        "query": query,
-        "results": filtered_results,
-    }
-    
-    return render(request, 'browser/results.html', context)
