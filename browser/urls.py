328974b96bac20ffeb82f59927f71f3828b3d72d
abakarovaMarina <abakamarina@gmail.com>
2024-12-20 00:35:48 +0100

    .

--- browser/urls.py
+++ browser/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 from .views import search_view, results_view, download_folder, upload_file, drosophiladb, job_running, serve_file
 
 urlpatterns = [
-    path('results/download/<str:fbpp_id>/', download_folder, name='download_file'),
     path('search/', search_view, name='search'),
     path('results/', results_view, name='results'),
     path('job_running/', job_running, name='job_running'),
     path('upload/', upload_file, name='upload_file'),
     path('drosophiladb/',drosophiladb, name='drosophiladb'),
     path('data/<str:folder>/<str:filename>/', serve_file, name='serve_file'),
+    path('download_folder/<str:fbpp_id>/', download_folder, name='download_folder'),
+
 ]
