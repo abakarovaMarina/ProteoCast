872b2e1dcbeb83171e5dcf5f6509c31180aa16de
abakarovaMarina <abakamarina@gmail.com>
2025-01-13 15:27:18 +0100

    residues

--- browser/views.py
+++ browser/views.py
@@ -176,20 +176,21 @@ def segmentation_dico(path_segF,path_bfactors):
 
     resi_70 = np.array(df_bfactors[df_bfactors['pLDDT'] <= 0.7].index)+1
     seg_dico=[]
-    for _, row in df_segmentation.loc[df_segmentation['type']=='GEMME'].iterrows():
-        if row['start'] in resi_70:
-            state = row['state']
-            if state == 1 or state == 2:
-                dico = {'residues': list(range(int(row['start']), int(row['end'])+1)),
-                        'color': dico_colors[state],
-                        'representation': 'putty',
-                        'representationColor': dico_colors[state]}
-                """dico={'start_residue_number':int(row['start']), 
-                    'end_residue_number':int(row['end']), 
-                    'color': dico_colors[state],
-                    'representation': 'putty',
-                    'representationColor': dico_colors[state]}"""
-                seg_dico.append(dico)
+    #for _, row in df_segmentation.loc[df_segmentation['type']=='GEMME'].iterrows():
+    #    if row['start'] in resi_70:
+    #        state = row['state']
+    #        if state == 1 or state == 2:
+    
+    """dico={'start_residue_number':int(row['start']), 
+        'end_residue_number':int(row['end']), 
+        'color': dico_colors[state],
+        'representation': 'putty',
+        'representationColor': dico_colors[state]}"""
+    dico = {'residues': list(range(1), int(100)+1),
+            'color': dico_colors[1],
+            'representation': 'putty',
+            'representationColor': dico_colors[1]}
+    seg_dico.append(dico)
     return seg_dico
 
 
