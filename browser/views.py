2957aa1bb4465f693bcb12496eb863e29227b153
abakarovaMarina <abakamarina@gmail.com>
2025-03-25 11:30:10 -0400

    error messages

--- browser/views.py
+++ browser/views.py
@@ -256,17 +256,15 @@ def results_view(request):
         if a3mtag:
             msa_file_job = msa_file_job[:-5]+'a3m'
         
-        if msa_file_job:
-            print('MSA')
-            msa_path = os.path.join(f'{alias_dir}/{id_folder}/2.ali', msa_file_job.split('.')[0]+'.fasta')
-            if os.path.exists(msa_path):
-                # Use grep to count the number of sequences
-                result = subprocess.run(['grep', '-c', '>', msa_path], capture_output=True, text=True, check=True)
-                sequences = int(result.stdout.strip())
-                print("MSA", sequences)
-                if sequences < 20:
-                    message = 'The submitted MSA contains too few sequences. For reliable predictions, the MSA should contain at least a couple of hundred sequences.'
-                    return render(request, 'browser/error.html', {'message': message}, status=500)
+        msa_path = f'/{alias_dir}/{id_folder}/2.ali'+msa_file_job.split('.')[0]+'.fasta'
+        if os.path.exists(msa_path):
+            # Use grep to count the number of sequences
+            result = subprocess.run(['grep', '-c', '>', msa_path], capture_output=True, text=True, check=True)
+            sequences = int(result.stdout.strip())
+            print("MSA", sequences)
+            if sequences < 20:
+                message = 'The submitted MSA contains too few sequences. For reliable predictions, the MSA should contain at least a couple of hundred sequences.'
+                return render(request, 'browser/error.html', {'message': message}, status=500)
     ## drosophila db
     else:
         # Only for the fly
