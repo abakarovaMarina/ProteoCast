1ddd780b049c5a381b3266df48c838ea8a735a70
abakarovaMarina <abakamarina@gmail.com>
2025-08-19 11:57:54 +0200

    pdb id correct

--- browser/views.py
+++ browser/views.py
@@ -291,8 +291,8 @@ def results_view(request):
         prot_id = None
         for file_name in files:
             if "ProteoCast" in file_name:
-                prot_id = '_'.join(file_name.split('.')[1].split('_')[:-1])  # Extract protein ID 
-            if ('10.' in file_name): 
+                prot_id = '_'.join(file_name.split('.')[1].split('_')[:-1])  # Extract protein ID
+            if ('10.' in file_name and ('ResClass' not in file_name or 'Sensitivity' not in file_name or 'ResRSA' not in file_name)):  # 10. in filename
                 pdb_id = file_name[:-4]
             if ('a3m' in file_name or 'a2m' in file_name):
                 a3mtag=file_name[-3:]
@@ -314,7 +314,7 @@ def results_view(request):
             return render(request, 'browser/error.html', {'message': message}, status=500)
 
 
-    ### Drosophila DB
+    ## drosophila db
     else:
         # Only for the fly
         data_path = '/data/Drosophila_ProteoCast/'
