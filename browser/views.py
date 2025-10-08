405e73ff2727fb3fcc27decbfb92520c67c3c6e8
abakarovaMarina <abakamarina@gmail.com>
2025-10-08 16:25:22 +0200

    check

--- browser/views.py
+++ browser/views.py
@@ -929,6 +929,8 @@ def results_view(request):
     ## segmentation data for 3D
     seg_dico = segmentation_dico(f'{data_path}{id_folder}/8.{prot_id}_Segmentation.csv', f'{data_path}{id_folder}/14.{prot_id}_GEMME_pLDDT.csv') 
     
+    if (prot_name[:3] == 'job'):
+        id_folder = 'job' +id_folder
 
     ## retrieve unaligned residues 
     if (prot_name[:3] == 'job' and os.path.exists(f'{data_path}{id_folder}/rsa_values.csv')):
