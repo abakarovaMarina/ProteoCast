42f9b5cbdeb01fdd343f6ed07735368442a88488
Marina Abakarova <abakamarina@gmail.com>
2026-04-27 12:25:43 +0200

    plotly corrections

--- MarinaWeb/settings.py
+++ MarinaWeb/settings.py
@@ -15,7 +15,26 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+#LOGGING = {
+#    "version": 1,
+#    "disable_existing_loggers": False,
+#    "handlers": {
+#        "console": {
+#            "class": "logging.StreamHandler",
+#        },
+#    },
+#    "root": {
+#        "handlers": ["console"],
+#        "level": "INFO",
+#    },
+#    "loggers": {
+#        "django.request": {
+#            "handlers": ["console"],
+#            "level": "ERROR",
+#            "propagate": True,
+#        },
+#    },
+#}
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
